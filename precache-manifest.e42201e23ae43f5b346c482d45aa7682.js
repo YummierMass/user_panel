2a74d586875d92484aa08544078499538e2f8609
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-28 15:43:43 +0000

    Deploy YummierMass/user_panel to github.com/YummierMass/user_panel.git:gh-pages

---
+++ precache-manifest.e42201e23ae43f5b346c482d45aa7682.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "785cd4661876fdf14835cc46e2aa3a0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0747e1aaaa108faa412",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4c7b3f5c509dc4f9298d",
+    "url": "/static/css/main.91acfd93.chunk.css"
+  },
+  {
+    "revision": "e0747e1aaaa108faa412",
+    "url": "/static/js/2.10e80657.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.10e80657.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c7b3f5c509dc4f9298d",
+    "url": "/static/js/main.3c3d6ea3.chunk.js"
+  },
+  {
+    "revision": "daa76ffcd4562c05159d",
+    "url": "/static/js/runtime-main.033b6b2c.js"
+  }
+]);
